595a3addc3048ec81f35b5d2f76a3ff3512a6d7f
fajrul <fajrul452@gmail.com>
2021-01-06 16:13:56 +0700

    session

---
+++ app.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const qrcode = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
+
+// Path where the session data will be stored
+const SESSION_FILE_PATH = './session.json';
+
+// Load the session data if it has been previously saved
+let sessionData;
+if(fs.existsSync(SESSION_FILE_PATH)) {
+    sessionData = require(SESSION_FILE_PATH);
+}
+
+// Use the saved values
+const client = new Client({
+    session: sessionData
+});
+
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    if (msg.body == '!ping') {
+        msg.reply('Helo Shofii :v ini kukirim dengan bot');
+    }
+});
+
+// Save session values to the file upon successful auth
+client.on('authenticated', (session) => {
+    sessionData = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
+
+client.initialize();
