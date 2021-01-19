b1ab048dfbed123e44f8646be6aae05f3ebf215e
fajrul <fajrul452@gmail.com>
2021-01-19 08:41:12 +0700

    Make different multiple and singgle

--- app-multiple.js
+++ app-multiple.js
@@ -9,13 +9,13 @@ const { phoneNumberFormatter } = require('./Helpers/formatter');
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname });
+  res.sendFile('multiple.html', { root: __dirname });
 });
 
 const sessions = [];
